6243899cd61616ca480bd798b3ae78f7415df5f2
Haeun <hek1303@naver.com>
2026-09-03 22:18:06 +0900

    [Passed] 알파벳에 따른 평가 (47ms, 16MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,8 @@
+E = input()
+
+if E == 'S': print('Superior')
+elif E == 'A': print('Excellent')
+elif E == 'B': print('Good')
+elif E == 'C': print('Usually')
+elif E == 'D': print('Effort')
+else: print('Failure')
